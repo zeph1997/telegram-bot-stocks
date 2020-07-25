8d0c91064682c9d0fde62d3af7d432058a26f4c8
zeph1997 <zeph1997@hotmail.com>
2020-07-25 10:26:53 +0800

    Update mainProg.py

--- mainProg.py
+++ mainProg.py
@@ -44,23 +44,6 @@ def add_stocks(message):
     else:
         bot.send_message(message.chat.id,"Wrong code entered. Please try again")
     
-    '''
-    Try doing recursive function to keep listening for the stock price until N is typed
-    '''
-    # bot.send_message(message.chat.id,"Please type in the ticker for the stocks to monitor one by one! Type \"N\" to stop")
-    # while(message)
-
-    '''
-    original code that works
-    '''
-    # bot.reply_to(message, "That's great, getting the Stock Price of DBS...")
-    # url = "https://sg.finance.yahoo.com/quote/D05.SI/"
-    # response = requests.get(url)
-    # soup = BeautifulSoup(response.content,"html.parser")
-    # price = soup.select("span[data-reactid*='14']")[0].text.strip()
-    # timing = soup.select("span[data-reactid*='18']")[0].text.strip()
-    # bot.send_message(message.chat.id,"$"+price+" per share, Last Checked "+timing)
-
 @bot.message_handler(commands=["getprice"])
 def get_price(messageO):
     message = ""
@@ -78,21 +61,6 @@ def get_price(messageO):
         timing = soup.select("span[data-reactid*='18']")[0].text.strip()
         message += f"Price of {i} \n${price}, {timing} \n"
     bot.send_message(messageO.chat.id,message)
-
-# @bot.message_handler(commands=["memethis"])
-# def find_meme(messageO):
-#     message = messageO.text.split()
-#     bot.send_message(messageO.chat.id,f"Getting the meme for {message[1]}")
-#     url = 
-#     response = requests.get(url)
-#     print(response.content)
-#     soup = BeautifulSoup(response.content,"html.parser")
-#     # shortlist = soup.find_all("table",class_="entry_list")
-#     print(shortlist)
-#     # manyData = json.dumps(data,sort_keys=True)
-#     # manyDataDict = eval(manyData)
-#     # bot.send_video(messageO.chat.id,manyDataDict["data"][0]["url"])
-#     # print(manyDataDict["data"][0]["url"])
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
